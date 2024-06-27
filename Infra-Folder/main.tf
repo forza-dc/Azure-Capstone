1e0328cbda29185cb164fef2c4e666fe0e84579e
S T <stsamdc@hotmail.com>
2024-06-27 16:34:11 +0000

    Updated main.tf

--- Infra-Folder/main.tf
+++ Infra-Folder/main.tf
@@ -9,7 +9,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "aks" {
-  name     = "ForzaCapstoneResourceGroup-java"
+  name     = "ForzaCapstoneResourceGroup"
   location = "East US"
 }
 
